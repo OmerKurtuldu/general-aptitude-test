9299b7a2f04f27b7f0787ea100dc5795d8c0b4d7
OmerKurtuldu <omerkurtuldu11@gmail.com>
2024-08-03 19:07:46 +0300

    feat(core): Implement core security and utility features
    - Added BaseSecurityService to manage common security configurations.
    - Integrated JwtAuthFilter for handling JWT authentication.
    - Defined whitelist URLs for public access.
    - Provided utility methods for handling role type conversions.

---
+++ core-package/src/main/java/com/gyt/corepackage/utils/constants/Paths.java
@@ -0,0 +1,17 @@
+package com.gyt.corepackage.utils.constants;
+
+public class Paths {
+    public static final String ConfigurationBasePackage = "com.gyt.corepackage";
+
+    public static class Management {
+        public static final String ServiceBasePackage = "com.gyt.managementservice";
+    }
+
+    public static class Question {
+        public static final String ServiceBasePackage = "com.gyt.questionservice";
+    }
+
+
+
+
+}
